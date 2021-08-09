81ed60f2bc875382abfdbdbe2c307d9974952e4c
TransientTetra <sperkowski.mikolaj@gmail.com>
2021-08-09 18:05:06 +0200

    Add progress bars for all macros

--- app/src/main/java/com/example/ezmacro/MainActivity.java
+++ app/src/main/java/com/example/ezmacro/MainActivity.java
@@ -10,6 +10,15 @@ public class MainActivity extends AppCompatActivity
 {
 	private ProgressBar caloriesProgressBar;
 	private TextView caloriesPercent;
+	
+	private ProgressBar proteinProgressBar;
+	private TextView proteinPercent;
+
+	private ProgressBar fatProgressBar;
+	private TextView fatPercent;
+
+	private ProgressBar carbProgressBar;
+	private TextView carbPercent;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -19,15 +28,44 @@ public class MainActivity extends AppCompatActivity
 
 		caloriesProgressBar = findViewById(R.id.caloriesProgressBar);
 		caloriesPercent = findViewById(R.id.caloriesProgressPercent);
+		proteinProgressBar = findViewById(R.id.proteinProgressBar);
+		proteinPercent = findViewById(R.id.proteinProgressPercent);
+		fatProgressBar = findViewById(R.id.fatProgressBar);
+		fatPercent = findViewById(R.id.fatProgressPercent);
+		carbProgressBar = findViewById(R.id.carbProgressBar);
+		carbPercent = findViewById(R.id.carbProgressPercent);
 
 		setCalorieProgress(39);
+		setProteinProgress(50);
+		setFatProgress(80);
+		setCarbProgress(120);
+	}
+
+	private void setSingleProgress(int percent, ProgressBar progressBar, TextView textView)
+	{
+		// todo what when over 100%?
+		percent = Math.max(percent, 0);
+		progressBar.setProgress(percent);
+		textView.setText(progressBar.getProgress() + "%");
 	}
 
 	public void setCalorieProgress(int percent)
 	{
-		// clamping between 0 and 100
-		percent = Math.min(Math.max(percent, 0), 100);
-		caloriesProgressBar.setProgress(percent);
-		caloriesPercent.setText(caloriesProgressBar.getProgress() + "%");
+		setSingleProgress(percent, caloriesProgressBar, caloriesPercent);
+	}
+
+	public void setProteinProgress(int percent)
+	{
+		setSingleProgress(percent, proteinProgressBar, proteinPercent);
+	}
+
+	public void setFatProgress(int percent)
+	{
+		setSingleProgress(percent, fatProgressBar, fatPercent);
+	}
+
+	public void setCarbProgress(int percent)
+	{
+		setSingleProgress(percent, carbProgressBar, carbPercent);
 	}
 }
